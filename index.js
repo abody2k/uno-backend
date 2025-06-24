b3e70aa8244a94e40d84cae6776a107e72221c76
Abdulrahman <Abdulrahman.7.2k@gmail.com>
2025-06-24 12:00:32 +0300

    made rooms

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ let io = require("socket.io")(server)
 const crypto = require("crypto");
 
 function getIPHash(ip) {
-  return crypto.createHash("sha256").update(ip).digest("hex");
+  return crypto.createHash("sha256").update(ip).digest("hex").slice(0, 12);
 }
 
 
@@ -24,8 +24,10 @@ function generateUserId() {
 
 io.on("connection",(client)=>{
 
-    //each player should have a unique ID stored on their device and is created upon starting the game
-    client.send("u"+ generateUserId());
+    // //each player should have a unique ID stored on their device and is created upon starting the game
+    // client.send("u"+ generateUserId());
+
+
     const ip =
     client.handshake.headers['x-forwarded-for']?.split(',').shift() ||
     client.conn.remoteAddress;
@@ -38,13 +40,20 @@ io.on("connection",(client)=>{
   
 
 
-    client.on("mr",(op)=>{
+    client.on("mr",(userHash)=>{
         
-        // generate a new roomID 
+        console.log("user is trying to make a new room");
+        console.log(userHash);
         
+        
+        //delete the room data of that user using his hash
         //put a timer on the room to delete it
         //make the player join the room
-        // create a new room on redis
+        if(!userHash)
+            return
+
+        client.join(userHash);
+        client.send("l")
         
 
     })
